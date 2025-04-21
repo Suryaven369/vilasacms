c1a28f5c15c56b6a411adcf2af26d2816c18cf5f
Suryaven369 <suryavenkatesh369@gmail.com>
2025-04-22 01:10:48 +0530

    Update Media.ts

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,18 +1,4 @@
-import type { CollectionConfig } from 'payload';
-import {
-  FixedToolbarFeature,
-  InlineToolbarFeature,
-  lexicalEditor,
-} from '@payloadcms/richtext-lexical';
-
-import { s3Storage } from '@payloadcms/storage-s3';
-import { anyone } from '../access/anyone';
-import { authenticated } from '../access/authenticated';
-
-const adapter = s3Storage({
-  config: {
-    endpoint: `https://${process.env.SUPABASE_PROJECT_REF}.supabase.co/storage/v1`,
-    credentials: {import type { CollectionConfig } from 'payload'
+import type { CollectionConfig } from 'payload'
 
 export const Media: CollectionConfig = {
   slug: 'media',
@@ -28,20 +14,22 @@ export const Media: CollectionConfig = {
   ],
   upload: true,
 }
+import type { CollectionConfig } from 'payload'
+import type { CollectionConfig } from 'payload'
 
-      accessKeyId: process.env.SUPABASE_ACCESS_KEY_ID || 'service_role',
-      secretAccessKey: process.env.SUPABASE_SECRET_ACCESS_KEY || ''
-    },
-    forcePathStyle: true,
-    region: 'us-east-1' // Supabase requires this but it doesn't matter what we set
-  },
-  bucket: process.env.SUPABASE_BUCKET || '',
-  collections: {
-    media: {
-      prefix: 'media'
-    }
-  }
-});
+import {
+  FixedToolbarFeature,
+  InlineToolbarFeature,
+  lexicalEditor,
+} from '@payloadcms/richtext-lexical'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+import { anyone } from '../access/anyone'
+import { authenticated } from '../access/authenticated'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
 
 export const Media: CollectionConfig = {
   slug: 'media',
@@ -55,21 +43,21 @@ export const Media: CollectionConfig = {
     {
       name: 'alt',
       type: 'text',
+      //required: true,
     },
     {
       name: 'caption',
       type: 'richText',
       editor: lexicalEditor({
-        features: [
-          FixedToolbarFeature(),
-          InlineToolbarFeature(),
-        ],
+        features: ({ rootFeatures }) => {
+          return [...rootFeatures, FixedToolbarFeature(), InlineToolbarFeature()]
+        },
       }),
     },
   ],
   upload: {
-    //
-    disableLocalStorage: true,
+    // Upload to the public/media directory in Next.js making them publicly accessible even outside of Payload
+    staticDir: path.resolve(dirname, '../../public/media'),
     adminThumbnail: 'thumbnail',
     focalPoint: true,
     imageSizes: [
@@ -106,4 +94,19 @@ export const Media: CollectionConfig = {
       },
     ],
   },
-};
+}
+
+export const Media: CollectionConfig = {
+  slug: 'media',
+  access: {
+    read: () => true,
+  },
+  fields: [
+    {
+      name: 'alt',
+      type: 'text',
+      required: true,
+    },
+  ],
+  upload: true,
+}
