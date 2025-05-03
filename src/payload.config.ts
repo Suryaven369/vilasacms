d4ffe098461b615596a097f8538d4f2f2c6bd8ed
Suryaven369 <suryavenkatesh369@gmail.com>
2025-05-03 13:30:06 +0530

    Update payload.config.ts

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -94,7 +94,7 @@ export default buildConfig({
     },
   }),
   collections: [Pages, Posts, Media, Categories, Users, Services, Testimonials, Portfolio],
-  cors: [https://www.viilasa.com, https://viilasasite.vercel.app],
+  cors: ["https://www.viilasa.com", "https://viilasasite.vercel.app"],
   globals: [Header, Footer],
   plugins: [
     ...plugins,
